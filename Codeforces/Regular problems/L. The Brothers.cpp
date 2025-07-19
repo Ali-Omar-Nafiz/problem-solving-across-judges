ad773a80e72b8f9243a06cf2b7f722dedae1b39f
Ali-Omar-Nafiz <aliomarnafizbd@gmail.com>
2025-07-19 13:29:42 +0600

    add and update

---
+++ Codeforces/Regular problems/L. The Brothers.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string f1,f2,s1,s2;
+    cin>>f1>>s1>>f2>>s2;
+    cout<<((s1==s2)?"ARE Brothers":"NOT");
+    return 0;
+}
